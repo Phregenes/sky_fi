d37c9ae9783c70ff9d1e13e1725dc43fb249b1b1
Pedro Regenes <phregenes@gmail.com>
2020-05-29 21:19:33 -0300

    feat add component menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+const Menu = () => 
+
+
+<div className="menu">
+<nav>
+  <a href="#">Home</a>
+  <a href="#">Links</a>
+  <a href="#">Fórum</a>
+  <div id="indicator"></div>
+</nav>
+</div>
+
+export default Menu;
